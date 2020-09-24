8442e496a6211cc9c72dc5412a34b914a58ff5c7
Mona Lisa <email@example.com>
2020-09-24 10:11:11 -0600

    final changes summary added

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -11,37 +11,31 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            
             var cells = line.Split(',');
 
-            // If your array.Length is less than 3, something went wrong
+            
             if (cells.Length < 3)
             {
-                // Log that and return null
+                
                 logger.LogError("Insufficient Information: Could not parse.", null);
-                // Do not fail if one record parsing fails, return null
-                return null; // TODO Implement
+                
+                return null; 
             }
 
-            // grab the latitude from your array at index 0
+            
             var lat = double.Parse(cells[0]);
-            // grab the longitude from your array at index 1
+            
             var longi = double.Parse(cells[1]);
-            // grab the name from your array at index 2
+            
             var name = cells[2];
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
-
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
+           
             var point = new Point();
             point.Latitude = lat;
             point.Longitude = longi;
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
+            
             TacoBell tacoBell = new TacoBell();
-            // Then, return the instance of your TacoBell class
-            // Since it conforms to ITrackable
+            
             tacoBell.Location = point;
             tacoBell.Name = name;
 
